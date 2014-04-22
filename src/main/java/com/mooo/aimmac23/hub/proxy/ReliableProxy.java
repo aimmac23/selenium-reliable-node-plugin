39b6323853219908abb6bfd83df33a1ff66654dd
Alasdair Macmillan <aimmac23@gmail.com>
2014-04-23 00:11:43 +0100

    Commit this too

--- src/main/java/com/mooo/aimmac23/hub/proxy/ReliableProxy.java
+++ src/main/java/com/mooo/aimmac23/hub/proxy/ReliableProxy.java
@@ -1,14 +1,10 @@
 package com.mooo.aimmac23.hub.proxy;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.openqa.grid.common.RegistrationRequest;
-import org.openqa.grid.internal.BaseRemoteProxy;
 import org.openqa.grid.internal.Registry;
 import org.openqa.grid.internal.TestSession;
 import org.openqa.grid.internal.TestSlot;
@@ -17,33 +13,14 @@ import org.openqa.grid.internal.utils.CapabilityMatcher;
 import org.openqa.grid.selenium.proxy.DefaultRemoteProxy;
 
 import com.mooo.aimmac23.hub.RemoteNodeTester;
-import com.mooo.aimmac23.hub.proxy.internal.ReliableTestSlot;
 
 public class ReliableProxy extends DefaultRemoteProxy implements SelfHealingProxy {
 	
 	private Set<Map<String, Object>> brokenCapabilities = new HashSet<Map<String, Object>>();
 	private Set<Map<String, Object>> workingCapabilities = new HashSet<Map<String, Object>>();
 	
-	
 	public ReliableProxy(RegistrationRequest request, Registry registry) {
 		super(request, registry);
-		
-		
-		try {
-			List<TestSlot> oldSlots = getTestSlots();
-			List<ReliableTestSlot> newSlots = new ArrayList<ReliableTestSlot>();
-			
-			for(TestSlot slot : oldSlots) {
-				newSlots.add(new ReliableTestSlot(this, slot.getProtocol(), slot.getPath(), slot.getCapabilities()));
-			}
-			
-			
-			Field slotFields = BaseRemoteProxy.class.getDeclaredField("testSlots");
-			slotFields.setAccessible(true);
-			slotFields.set(this, newSlots);
-		} catch(Exception e) {
-			throw new IllegalStateException(e);
-		}		
 	}
 	
 	@Override
@@ -95,34 +72,4 @@ public class ReliableProxy extends DefaultRemoteProxy implements SelfHealingProx
 	public CapabilityMatcher getCapabilityHelper() {
 		return new ReliableCapabilityMatcher(this, super.getCapabilityHelper());
 	}
-	
-	public static class ReliableCapabilityMatcher implements CapabilityMatcher {
-		
-		private CapabilityMatcher innerMatcher;
-		private ReliableProxy proxy;
-
-		public ReliableCapabilityMatcher(ReliableProxy proxy, CapabilityMatcher innerMatcher) {
-			this.proxy = proxy;
-			this.innerMatcher = innerMatcher;
-		}
-		
-		@Override
-		public boolean matches(Map<String, Object> currentCapability,
-				Map<String, Object> requestedCapability) {
-			
-			// to allow the node to be tested
-			if(requestedCapability.containsKey("proxyId")) {
-				return innerMatcher.matches(currentCapability, requestedCapability);
-			}
-			
-			// check to see if we're allowed to use that config
-			if(!proxy.isCapabilityUsable(currentCapability)) {
-				return false;
-			}
-			
-			// otherwise, match on the capabilities
-			return innerMatcher.matches(currentCapability, requestedCapability);
-		}
-	}
-
 }
